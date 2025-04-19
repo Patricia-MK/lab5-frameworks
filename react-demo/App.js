9ba7e57b19c2ed9a6fbde8b6517e33b9a395a002
Patricia-MK <patricia.kashweka@cs.unza.zm>
2025-04-19 14:42:49 +0200

    Used existing index.html for Bootstrap, added Tailwind and React demo

--- react-demo/App.js
+++ react-demo/App.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
 
-function WelcomeMessage({ name }) {
+function Team({ name }) {
   return <h1>Welcome, {name}!</h1>;
 }
 
-function App() {
-  const [showMsg, setShowMsg] = useState(true);
+export default function App() {
+  const [showTeam, setShowTeam] = useState(true);
 
   return (
     <div className="App">
-      <WelcomeMessage name="Patricia" />
-      <button onClick={() => setShowMsg(!showMsg)}>
-        {showMsg ? "Hide" : "Show"} Message
+      <Team name="Patricia" />
+      <button onClick={() => setShowTeam(!showTeam)}>
+        {showTeam ? "Hide Team" : "Show Team"}
       </button>
-      {showMsg && <p>This is a toggleable message!</p>}
+      {showTeam && (
+        <ul>
+          <li>Patricia Mboma Kashweka</li>
+          <li>Chaabilo Lubobya </li>
+        </ul>
+      )}
     </div>
   );
 }
-
-export default App;
